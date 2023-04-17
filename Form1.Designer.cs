1750bf41bf33e5fc9fdf6a7559d1e353e64dd9b2
Jim Rutt <jimrutt@gmail.com>
2023-04-17 09:38:12 -0400

    opened on 246

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,20 +30,20 @@
         {
             this.Movie = new System.Windows.Forms.TabControl();
             this.MovieTab = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ScenesList = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MovieText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MovieHintText = new System.Windows.Forms.TextBox();
             this.ScenesTab = new System.Windows.Forms.TabPage();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.CharactersTab = new System.Windows.Forms.TabPage();
             this.Settings = new System.Windows.Forms.TabPage();
-            this.MovieHintText = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.MovieText = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.ScenesList = new System.Windows.Forms.ListBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.Movie.SuspendLayout();
             this.MovieTab.SuspendLayout();
             this.ScenesTab.SuspendLayout();
@@ -81,6 +81,84 @@
             this.MovieTab.UseVisualStyleBackColor = true;
             this.MovieTab.Click += new System.EventHandler(this.TabPage1_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Scenes";
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // ScenesList
+            // 
+            this.ScenesList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScenesList.FormattingEnabled = true;
+            this.ScenesList.ItemHeight = 20;
+            this.ScenesList.Location = new System.Drawing.Point(24, 55);
+            this.ScenesList.Name = "ScenesList";
+            this.ScenesList.Size = new System.Drawing.Size(183, 404);
+            this.ScenesList.TabIndex = 5;
+            this.ScenesList.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(561, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Make Movie Text                                                                  " +
+    "                                                                                " +
+    "    ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(557, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Movie Text";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // MovieText
+            // 
+            this.MovieText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MovieText.Location = new System.Drawing.Point(561, 273);
+            this.MovieText.Multiline = true;
+            this.MovieText.Name = "MovieText";
+            this.MovieText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MovieText.Size = new System.Drawing.Size(538, 251);
+            this.MovieText.TabIndex = 2;
+            this.MovieText.TextChanged += new System.EventHandler(this.MovieHintText_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(551, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Movie Text Hints";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // MovieHintText
+            // 
+            this.MovieHintText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MovieHintText.Location = new System.Drawing.Point(555, 36);
+            this.MovieHintText.Multiline = true;
+            this.MovieHintText.Name = "MovieHintText";
+            this.MovieHintText.Size = new System.Drawing.Size(545, 153);
+            this.MovieHintText.TabIndex = 0;
+            this.MovieHintText.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
             // ScenesTab
             // 
             this.ScenesTab.Controls.Add(this.tabControl1);
@@ -115,7 +193,7 @@
             // 
             // tabPage4
             // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 34);
+            this.tabPage4.Location = new System.Drawing.Point(4, 40);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage4.Size = new System.Drawing.Size(0, 0);
@@ -143,62 +221,6 @@
             this.Settings.Text = "Settings";
             this.Settings.UseVisualStyleBackColor = true;
             // 
-            // MovieHintText
-            // 
-            this.MovieHintText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MovieHintText.Location = new System.Drawing.Point(555, 36);
-            this.MovieHintText.Multiline = true;
-            this.MovieHintText.Name = "MovieHintText";
-            this.MovieHintText.Size = new System.Drawing.Size(545, 153);
-            this.MovieHintText.TabIndex = 0;
-            this.MovieHintText.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(551, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(166, 24);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Movie Text Hints";
-            this.label1.Click += new System.EventHandler(this.Label1_Click);
-            // 
-            // MovieText
-            // 
-            this.MovieText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MovieText.Location = new System.Drawing.Point(561, 273);
-            this.MovieText.Multiline = true;
-            this.MovieText.Name = "MovieText";
-            this.MovieText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.MovieText.Size = new System.Drawing.Size(538, 282);
-            this.MovieText.TabIndex = 2;
-            this.MovieText.TextChanged += new System.EventHandler(this.MovieHintText_TextChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(557, 246);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 24);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Movie Text";
-            this.label2.Click += new System.EventHandler(this.Label2_Click);
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(561, 195);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(214, 31);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Make Movie Text                                                                  " +
-    "                                                                                " +
-    "    ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
-            // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -209,28 +231,6 @@
             this.button3.Text = "Exit";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.Button3_Click);
-            // 
-            // ScenesList
-            // 
-            this.ScenesList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ScenesList.FormattingEnabled = true;
-            this.ScenesList.ItemHeight = 20;
-            this.ScenesList.Location = new System.Drawing.Point(24, 55);
-            this.ScenesList.Name = "ScenesList";
-            this.ScenesList.Size = new System.Drawing.Size(183, 404);
-            this.ScenesList.TabIndex = 5;
-            this.ScenesList.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(20, 28);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 24);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Scenes";
-            this.label3.Click += new System.EventHandler(this.Label3_Click);
             // 
             // Form1
             // 
